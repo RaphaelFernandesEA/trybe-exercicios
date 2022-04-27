47b28c4de607b66c10ecb494d59c916528e8542a
RaphaelFernandes <raphael1454@hotmail.com>
2022-04-27 18:11:05 -0300

    Exercicios Hello World do primeiro dia do Bloco 10

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-1e2/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const compromissos = ['Solar System Project', 'Project Tryunfo', 'Project TrybeTunes', 'Frontend Online Store Project']
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        {compromissos.map((tarefa) => Task(tarefa))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
