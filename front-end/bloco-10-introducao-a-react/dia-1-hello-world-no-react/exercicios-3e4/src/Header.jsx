3fb38d204dbf1ccb7ae4328d31f2bff44d41d409
RaphaelFernandes <raphael1454@hotmail.com>
2022-05-11 09:52:15 -0300

    melhorando código dos exercicios

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-3e4/src/Header.jsx
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-3e4/src/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class Header extends React.Component{
+class Header extends React.Component {
     render(){
         return(<h1 className='title'>Conteúdos de Front-End</h1>)
     }
