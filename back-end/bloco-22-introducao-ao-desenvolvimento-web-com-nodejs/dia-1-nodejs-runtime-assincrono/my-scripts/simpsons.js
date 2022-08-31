a0d498d55d10e6bacb717bd6e47f642ba210accd
RaphaelFernandes <raphael1454@hotmail.com>
2022-08-31 11:09:13 -0300

    exercicios dia 22.1 completos

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/simpsons.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincrono/my-scripts/simpsons.js
@@ -17,6 +17,7 @@ const data = async () => {
 
 
 
+
 // const findById = async (param) => {
 //   const objFiles = await data();
 //   // console.log(objFiles)
@@ -37,6 +38,7 @@ const data = async () => {
 
 
 
+
 // const removeCharacters = async () => {
 //   const result = await data();
 //   // console.log(result);
@@ -54,3 +56,45 @@ const data = async () => {
 // reWriteFile()
 
 
+
+
+// const createNewFile = async () => {
+//   const objFiles = await data();
+//   result = objFiles.filter(({ id }) => Number(id) <= 4)
+//   console.log(result)
+//   await fs.writeFile('./simpsonFamily.json', JSON.stringify(result));
+// }
+
+// createNewFile();
+
+
+
+// const addNelsonMuntz = async () => {
+//   const objFiles = await data();
+//   const nelsonObj = objFiles.find(({ name }) => name === "Nelson Muntz")
+//   // console.log(nelsonObj);
+
+//   const simpsonFamilyContent = await fs.readFile('./simpsonFamily.json', 'utf-8');
+//   const simpsonFamily = JSON.parse(simpsonFamilyContent);
+//   // console.log(simpsonFamily)
+//   const newContent = simpsonFamily.concat(nelsonObj);
+//   console.log(newContent);
+//   await fs.writeFile('./simpsonFamily.json', JSON.stringify(newContent))
+
+// }
+
+// addNelsonMuntz();
+
+
+
+const replaceNelson = async () => {
+  const simpsonFamilyContent = await fs.readFile('./simpsonFamily.json', 'utf-8');
+  const simpsonFamilyWithNelson = JSON.parse(simpsonFamilyContent);
+  const simpsonFamily = simpsonFamilyWithNelson.filter((element) => element.name !== "Nelson Muntz");
+  console.log(simpsonFamily)
+  const completeSimpsonFamily = simpsonFamily.concat({ id: '5', name: 'Maggie Simpson' });
+  console.log(completeSimpsonFamily);
+  fs.writeFile('./simpsonFamily.json', JSON.stringify(completeSimpsonFamily));
+}
+
+replaceNelson();
