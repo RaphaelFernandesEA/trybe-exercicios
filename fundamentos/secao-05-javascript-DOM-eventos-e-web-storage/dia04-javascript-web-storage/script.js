dcc6eae1e99e01e22a92a446ac1d4efde3f58c75
Raphael <raphael1454@hotmail.com>
2023-01-28 16:30:34 -0300

    Exercicios finalizados

--- fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia04-javascript-web-storage/script.js
+++ fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia04-javascript-web-storage/script.js
@@ -15,16 +15,16 @@
 // Referência do leitor de conteúdos.
 // Esse conteúdo escrito pode ser uma página de livro, uma reportagem de revista ou uma nota de jornal online. Para que você não tenha que pensar no conteúdo da página, utilize este link para gerar o texto para sua página.
 
-const body = document.body
+const div = document.getElementById("elemento-pai")
 
 const ulColorsBck = document.createElement('ul');
 ulColorsBck.innerText = "Cor de fundo";
-body.appendChild(ulColorsBck);
+div.appendChild(ulColorsBck);
 
 const colorsName = ["white", "black", "green", "blue", "yellow", "red"];
 
 const changeBackGroundColor = (color) => {
-  body.style.backgroundColor = color;
+  div.style.backgroundColor = color;
   localStorage.setItem("backgroundColor", color)
 }
 
@@ -41,13 +41,12 @@ colorsName.map((color) => {
 
 const ulFonts = document.createElement("ul");
 ulFonts.innerText = "Cor da fonte";
-body.appendChild(ulFonts);
+div.appendChild(ulFonts);
 
 const fontColors = [ "black", "red", "white"];
 
 const changefontColor = (color) => {
   let content = document.getElementsByTagName("p")
-  console.log(content);
   for (let index = 0; index < content.length; index += 1) {
     content[index].style.color = color;   
   }
@@ -67,60 +66,102 @@ fontColors.map((color) => {
 
 const ulFontSize = document.createElement("ul");
 ulFontSize.innerText = "Tamanho da fonte";
-body.appendChild(ulFontSize);
+div.appendChild(ulFontSize);
 
 const fontSize = ["8pt", "10pt", "12pt", "14pt", "20pt"];
+
+const changeFontSize = (size) => {
+  div.style.fontSize = size;
+  localStorage.setItem("fontSize", size)
+}
 
 fontSize.map((color) => {
   let fontSize = document.createElement('li');
   fontSize.className = "fontSizes-list"
   fontSize.style.listStyleType = "none";
-  ulFontSize.appendChild(fontSize);
   let button = document.createElement('button');
-  fontSize.appendChild(button);
   button.innerText = color;
+  button.addEventListener("click", (event) => changeFontSize(event.target.innerText))
+  fontSize.appendChild(button);
+  ulFontSize.appendChild(fontSize);
 });
 
 const ulSpace = document.createElement("ul");
 ulSpace.innerText = "Espaçamento entre linhas";
-body.appendChild(ulSpace);
+div.appendChild(ulSpace);
 
 const space = ["1", "normal", "1.5", "2", "3"];
+
+const changeLineHeight = (space) => {
+  let content = document.getElementsByTagName("p")
+  for (let index = 0; index < content.length; index += 1) {
+    content[index].style.lineHeight = space;   
+  }
+  localStorage.setItem("lineHeight", space);
+}
 
 space.map((color) => {
   let fontSpace = document.createElement('li');
   fontSpace.className = "fontSpaces-list"
   fontSpace.style.listStyleType = "none";
-  ulSpace.appendChild(fontSpace);
   let button = document.createElement('button');
-  fontSpace.appendChild(button);
   button.innerText = color;
+  button.addEventListener("click", (e) => changeLineHeight(e.target.innerText))
+  fontSpace.appendChild(button);
+  ulSpace.appendChild(fontSpace);
 });
 
 const ulFontType = document.createElement("ul");
 ulFontType.innerText = "Tipo de fonte";
-body.appendChild(ulFontType);
+div.appendChild(ulFontType);
 
 const fontType = ["Arial", "Times New Roman"];
+
+const changeFontFamily = (type) => {
+  div.style.fontFamily = type;
+  localStorage.setItem("fontFamily", type);
+}
 
 fontType.map((color) => {
   let fontType = document.createElement('li');
   fontType.className = "fontTypes-list"
   fontType.style.listStyleType = "none";
-  ulFontType.appendChild(fontType);
   let button = document.createElement('button');
-  fontType.appendChild(button);
   button.innerText = color;
+  button.addEventListener("click", (e) => changeFontFamily(e.target.innerText));
+  fontType.appendChild(button);
+  ulFontType.appendChild(fontType);
 })
 
 const content = () => {
   const p = document.createElement("p")
-  body.appendChild(p);
+  div.appendChild(p);
   p.innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 }
 
 content();
+content();
+content();
+content();
 
+const initialize = () => {
+  const fontSize = localStorage.getItem("fontSize");
+  changeFontSize(fontSize);
+
+  const lineHeight = localStorage.getItem("lineHeight");
+  changeLineHeight(lineHeight);
+
+  const fontFamily = localStorage.getItem("fontFamily");
+  changeFontFamily(fontFamily);
+
+  const backgroundColor = localStorage.getItem("backgroundColor");
+  changeBackGroundColor(backgroundColor);
+  
+  const fontColor = localStorage.getItem("fontColor");
+  changefontColor(fontColor);
+}
+
+initialize();
 
 
 
